cba3bc774d642f73e5534215463206c7b3808a0d
konrad9011 <konradd990212@gmail.com>
2023-09-17 22:34:42 +0200

    fix: register DelegatingHandler

--- src/Services/Projects/Projects.Application/Projects/Queries/GetProjectsQueryValidator.cs
+++ src/Services/Projects/Projects.Application/Projects/Queries/GetProjectsQueryValidator.cs
@@ -7,7 +7,7 @@ public class GetProjectsQueryValidator : AbstractValidator<GetProjectsQuery>
     public GetProjectsQueryValidator()
     {
         RuleFor(x => x.Take).NotEmpty().GreaterThan(0).LessThan(100);
-        RuleFor(x => x.Skip).NotEmpty().GreaterThanOrEqualTo(0);
+        RuleFor(x => x.Skip).NotNull().GreaterThanOrEqualTo(0);
         RuleFor(x => x.OrderBy);
         RuleFor(x => x.Filter);
         RuleFor(x => x.SelectedColumns);
